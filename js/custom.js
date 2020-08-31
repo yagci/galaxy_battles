11c060836e6770f79f38e9589f42cd613eda9dd5
Yagci <yagcinurce@gmail.com>
2020-08-31 17:20:20 +0200

    use ajax load() to import static html
    - added full jquery to get access to ajax load
    - created static_html folder
    - loading background svgs, footer and game title from static_html on each page
    
    - created loadStaticHTML function

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,9 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+function loadStaticHTML() {
+  $('#svg-container').load('static_html/game_title.html');
+  $('#background').load('static_html/background.html');
+  $('#footer').load('static_html/footer.html');
+}
